68a1a38a5754f617195f81f3fe4ac490a23f51f9
unknown <nanateinumonde@gmail.com>
2022-09-30 11:04:58 +0100

    Refactored sslyze scan json output

--- apis/scan_reports/tools/sslyze.py
+++ apis/scan_reports/tools/sslyze.py
@@ -79,61 +79,75 @@ class sslyze(Scanner):
         retrieve list of ports from the result
         """
         
-        if sslyze_result["server_scan_results"][0]["scan_status"] == "ERROR_NO_CONNECTIVITY":
-            # No connection was made with the server
+        try:
+        
+            if sslyze_result["server_scan_results"][0]["scan_status"] == "ERROR_NO_CONNECTIVITY":
+                # No connection was made with the server
+                
+                # delete the created file if an error occured.
+                subprocess.run(f'rm -f {self.output_file}',
+                            capture_output=True,
+                            shell=True,
+                            check=True)
+                
+                return {"Response":"ERROR_NO_CONNECTIVITY"}
             
+            elif sslyze_result["server_scan_results"][0]["scan_status"] == "COMPLETED":
+                
+                if sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["status"] == "COMPLETED":
+                
+                    result = {
+                        "ip_address": sslyze_result["server_scan_results"][0]["server_location"]["ip_address"],
+                        "port": sslyze_result["server_scan_results"][0]["server_location"]["port"],
+                        "connection_type": sslyze_result["server_scan_results"][0]["server_location"]["connection_type"],
+                        "scan_status": sslyze_result["server_scan_results"][0]["scan_status"],
+                        "uuid": sslyze_result["server_scan_results"][0]["uuid"],
+                        "date_scans_completed": sslyze_result["date_scans_completed"],
+                        "date_scans_started": sslyze_result["date_scans_started"],
+                        "connectivity_error_trace": sslyze_result["server_scan_results"][0]["connectivity_error_trace"],
+                        "connectivity_result": sslyze_result["server_scan_results"][0]["connectivity_result"],
+                        "connectivity_status": sslyze_result["server_scan_results"][0]["connectivity_status"],
+                        "network_configuration": sslyze_result["server_scan_results"][0]["network_configuration"],
+                        "scan_result": {
+                            "certificate_info": {
+                                "error_reason": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["error_reason"],
+                                "error_trace": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["error_trace"],
+                                "result": {
+                                    "certificate_deployments": {
+                                        "leaf_certificate_has_must_staple_extension": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0][ "leaf_certificate_has_must_staple_extension"],
+                                        "leaf_certificate_is_ev": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["leaf_certificate_is_ev"],
+                                        "leaf_certificate_signed_certificate_timestamps_count": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["leaf_certificate_signed_certificate_timestamps_count"],
+                                        "leaf_certificate_subject_matches_hostname": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["leaf_certificate_subject_matches_hostname"],
+                                        "ocsp_response": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["ocsp_response"],
+                                        "ocsp_response_is_trusted": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["ocsp_response_is_trusted"],
+                                        } 
+                                },
+                                "status": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["status"],
+                            }
+                        }
+                        
+                    }
+                else:
+                    subprocess.run(f'rm -f {self.output_file}',
+                        capture_output=True,
+                        shell=True,
+                        check=True)
+                    
+                    return {"Response":"SCAN_RESULT_CERTIFICATE_INFO_ERROR"}
+                
+        except KeyError as e:
             # delete the created file if an error occured.
             subprocess.run(f'rm -f {self.output_file}',
                         capture_output=True,
                         shell=True,
                         check=True)
+
+            logger.error("Key Error")
+            logger.error(e)
+            return {"Response":f"Scan result does not contain {e}"}
             
-            return {"Response":"ERROR_NO_CONNECTIVITY"}
-        
-        
-        elif sslyze_result["server_scan_results"][0]["scan_status"] == "COMPLETED":
-            
-            if sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["status"] == "COMPLETED":
-            
-                result = {
-                    "ip_address": sslyze_result["server_scan_results"][0]["server_location"]["ip_address"],
-                    "port": sslyze_result["server_scan_results"][0]["server_location"]["port"],
-                    "connection_type": sslyze_result["server_scan_results"][0]["server_location"]["connection_type"],
-                    "scan_status": sslyze_result["server_scan_results"][0]["scan_status"],
-                    "uuid": sslyze_result["server_scan_results"][0]["uuid"],
-                    "date_scans_completed": sslyze_result["date_scans_completed"],
-                    "date_scans_started": sslyze_result["date_scans_started"],
-                    "connectivity_error_trace": sslyze_result["server_scan_results"][0]["connectivity_error_trace"],
-                    "connectivity_result": sslyze_result["server_scan_results"][0]["connectivity_result"],
-                    "connectivity_status": sslyze_result["server_scan_results"][0]["connectivity_status"],
-                    "network_configuration": sslyze_result["server_scan_results"][0]["network_configuration"],
-                    "scan_result": {
-                        "certificate_info": {
-                            "error_reason": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["error_reason"],
-                            "error_trace": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["error_trace"],
-                            "result": {
-                                "certificate_deployments": {
-                                    "leaf_certificate_has_must_staple_extension": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0][ "leaf_certificate_has_must_staple_extension"],
-                                    "leaf_certificate_is_ev": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["leaf_certificate_is_ev"],
-                                    "leaf_certificate_signed_certificate_timestamps_count": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["leaf_certificate_signed_certificate_timestamps_count"],
-                                    "leaf_certificate_subject_matches_hostname": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["leaf_certificate_subject_matches_hostname"],
-                                    "ocsp_response": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["ocsp_response"],
-                                    "ocsp_response_is_trusted": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["result"]["certificate_deployments"][0]["ocsp_response_is_trusted"],
-                                    } 
-                            },
-                            "status": sslyze_result["server_scan_results"][0]["scan_result"]["certificate_info"]["status"],
-                        }
-                    }
-                    
-                }
-            else:
-                return {"Response":"SCAN_RESULT_CERTIFICATE_INFO_ERROR"}
-        
         self.data.append(result)
         
         return self.data
         
-        
-         
-        
-        
+     
