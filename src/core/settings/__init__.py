ffc9950a0199ac75dbcd68e80cfccb19fd3cf678
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-10-03 23:24:04 +0100

    moved project to src, installed and configured celery

--- src/core/settings/__init__.py
+++ src/core/settings/__init__.py
@@ -1,8 +1,14 @@
 """
 Initialize settings environment.
+for celery module: This will make sure the app is always imported when
+    Django starts so that shared_task will use this app.
 """
-from .base import base_env_config
 
+from .base import base_env_config
+from .celery import app as celery_app
+
+
+__all__ = ('celery_app',)
 
 def get_settings_environment():
     # detect environment to load settings configuration
