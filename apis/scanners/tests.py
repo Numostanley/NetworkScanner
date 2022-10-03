d946ae651812dd990e604a1a88b44e0efeb07b06
unknown <nanateinumonde@gmail.com>
2022-10-03 13:46:21 +0100

    Refactored test cases

--- apis/scanners/tests.py
+++ apis/scanners/tests.py
@@ -5,21 +5,21 @@ from django.urls import reverse
 class CVEScannerTest(TestCase):
 
     def test_CVEscanner(self):
-        view = self.client.get('/api/v1/cve-scan?ip_address=193.122.66.53')
+        view = self.client.get('/api/v1/scanners/cve-scan?ip_address=193.122.66.53')
         self.assertEqual(view.status_code, 200)
        
        
 class SslyzeTest(TestCase):
      
     def test_sslyze(self):
-        view = self.client.get('/api/v1/sslyze-scan?ip_address=193.122.67.133')
+        view = self.client.get('/api/v1/scanners/sslyze-scan?ip_address=193.122.67.133')
         self.assertEqual(view.status_code, 200)
         
         
 class WapitiTest(TestCase):
     
     def test_wapiti(self):
-        view = self.client.get('/api/v1/wapiti-scan?ip_address=193.122.66.53')
+        view = self.client.get('/api/v1/scanners/wapiti-scan?ip_address=193.122.66.53')
         self.assertEqual(view.status_code, 200)
         
         
