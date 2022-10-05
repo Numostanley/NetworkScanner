6c7ce11a9a0ac54aff40a04c5b3ddfe27d2867ff
unknown <nanateinumonde@gmail.com>
2022-10-05 21:17:41 +0100

    Added code to populate database in apis.scanners.tasks

--- src/apis/scanners/tasks.py
+++ src/apis/scanners/tasks.py
@@ -3,6 +3,10 @@ register scanners' tasks for celery to autodiscover
 """
 
 from celery import shared_task
+from .models.wapiti import Wapiti
+from .models.base import Host
+from django.core.exceptions import ObjectDoesNotExist
+import json
 
 from apis.scanners.tools import cvescanner, dirby, sslyze, wafwoof, wapiti, whatweb
 
@@ -19,7 +23,11 @@ def dirby_task(ip_address: str):
 
 @shared_task
 def sslyze_task(ip_address: str):
-    return sslyze.SslyzeScanner(ip_address).response()
+    
+    task = sslyze.SslyzeScanner(ip_address)
+    data = task.response()
+    
+    return data
 
 
 @shared_task
@@ -29,9 +37,26 @@ def wafwoof_task(ip_address: str):
 
 @shared_task
 def wapiti_task(ip_address: str):
-    return wapiti.WapitiScanner(ip_address).response()
+    
+    task = wapiti.WapitiScanner(ip_address)
+    data = task.response()
+    
+    try:
+        host = Host.objects.get(ip_address=ip_address)
+    except ObjectDoesNotExist:
+        host = Host.create_host(
+            ip_address=ip_address
+        )
+        
+    data_cleaned = json.loads(data)
+    
+    Wapiti.create_wapiti_scan(host=host, data=data_cleaned)
+    
+    return data_cleaned
 
 
 @shared_task
 def whatweb_task(ip_address: str):
     return whatweb.WhatWebScanner(ip_address).response()
+
+
