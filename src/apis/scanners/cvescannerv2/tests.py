a084bb310759898b445bf4056f5eb0e858171044
unknown <nanateinumonde@gmail.com>
2022-10-09 17:52:40 +0100

    Updated testcases and added test_db_data directory

--- src/apis/scanners/cvescannerv2/tests.py
+++ src/apis/scanners/cvescannerv2/tests.py
@@ -1,10 +1,41 @@
 from django.test import TestCase
 
 from apis.scanners.base.tests import BASE_URL
+from .models import CVEScannerV2
+from apis.scanners.hosts.models import Host
+import json
+
 
 
 class CVEScannerTest(TestCase):
 
+    def setUp(self):
+        
+        host = Host.create_host(ip_address='193.122.66.53')
+       
+        with open('test_db_data/cvescannerv2.json', 'r') as f:
+           json_data = f.read()
+           
+           data = json.loads(json_data)
+         
+        for datum in data:
+            CVEScannerV2.create_cvescanner_scan(host=host, data=datum['fields']['cve_data']['cve_data'])  
+
+
     def test_cvescanner(self):
-        view = self.client.get(f'{BASE_URL}/cvescanner/scan?ip_address=193.122.66.53')
-        self.assertEqual(view.status_code, 200)
+        response = self.client.get(f'{BASE_URL}/cvescanner/scan?ip_address=193.122.66.53')
+        self.assertEqual(response.status_code, 200)
+        
+    
+    def test_scan_result(self):
+        response = self.client.get(f'{BASE_URL}/cvescanner/get-result?ip_address=193.122.66.53')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_model_field_type(self):
+        cvescanner= CVEScannerV2.objects.get(id=1)
+        
+        cve_data= cvescanner._meta.get_field('cve_data').get_internal_type()
+        self.assertEqual(cve_data, 'JSONField')
+        
+        
