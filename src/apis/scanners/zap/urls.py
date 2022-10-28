7a21e441fe0cdb1652f4522ee6d7af171f452c57
unknown <nanateinumonde@gmail.com>
2022-10-28 14:12:47 +0100

    Added APIView to get zap scan data from DB

--- src/apis/scanners/zap/urls.py
+++ src/apis/scanners/zap/urls.py
@@ -7,5 +7,5 @@ app_name = "zap"
 urlpatterns = [
     # zap urls
     path('scan', views.ZapScannerAPIView.as_view()),
-    # path('get-result', views.SSLyzeScanResultAPIView.as_view()),
+    path('get-result', views.ZapScanResultAPIView.as_view()),
 ]
--- src/apis/scanners/zap/views.py
+++ src/apis/scanners/zap/views.py
@@ -38,3 +38,32 @@ class ZapScannerAPIView(AuthProtectedAPIView):
             error_logs.logger.error('ZapAPIView.get@Error')
             error_logs.logger.error(e)
             return responses.http_response_500('An error occurred!')
+
+class ZapScanResultAPIView(AuthProtectedAPIView):
+    
+    def get(self, request, *args, **kwargs):
+        query_params = request.query_params
+
+        # get host key from the url query parameters
+        if 'host' not in query_params:
+            return responses.http_response_400('Host key not found in query parameters!')
+
+        # get host value from the url query parameters
+        host_key = query_params.get('host', '')
+        if not host_key:
+            return responses.http_response_400('Host not specified!')
+
+        host = Host.get_host(host_key)
+        if not host:
+            return responses.http_response_404('Host not found!')
+
+        zap_data = Zap.get_zap_scan_by_host(host)
+        
+        if zap_data.count() < 1:
+            return responses.http_response_404("No scan result exists for this host.")
+
+        if zap_data.count() > 0:
+            return responses.http_response_200('Data successfully retrieved', zap_data)
+
+        return responses.http_response_500('An error occurred!')
+    
