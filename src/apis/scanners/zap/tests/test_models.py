e3cf98f9ad6c0cbe864028cc84468193ea40c445
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-10-28 18:36:17 +0100

    updated test_views and test_models

---
+++ src/apis/scanners/zap/tests/test_models.py
@@ -0,0 +1,7 @@
+import json
+
+from django.test import TestCase
+from django.db.models import QuerySet
+
+from apis.scanners.hosts.models import Host
+from apis.scanners.zap.models import Zap
---
+++ src/apis/scanners/zap/tests/test_views.py
@@ -0,0 +1,7 @@
+import json
+
+from django.test import TestCase
+
+from apis.scanners.base.tests import BASE_URL
+from apis.scanners.hosts.models import Host
+from apis.scanners.zap.models import Zap
