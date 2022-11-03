58b1254d4796657f72ae788a58ed4e633ae78f7d
unknown <nanateinumonde@gmail.com>
2022-11-03 20:15:12 +0100

    Removed s3fs environment variables

--- src/core/extras/env_vars.py
+++ src/core/extras/env_vars.py
@@ -8,11 +8,3 @@ from core.settings.base import env_config
 
 # R
 REDIS_URL = env_config['REDIS_URL']
-
-# S
-S3_ACCESS_KEY = env_config['S3_ACCESS_KEY']
-S3_BUCKET_NAME = env_config['S3_BUCKET_NAME']
-S3_DIR_PATH = env_config['S3_DIR_PATH']
-S3_NAMESPACE = env_config['S3_NAMESPACE']
-S3_REGION = env_config['S3_REGION']
-S3_SECRET_KEY = env_config['S3_SECRET_KEY']
